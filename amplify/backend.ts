93d1d4917132b5bc411e856611a6c0ad4eb620b7
rjabhi <rjabhi@amazon.com>
2025-01-13 13:53:49 -0800

    init commit

---
+++ amplify/backend.ts
@@ -0,0 +1,34 @@
+import { RemovalPolicy } from "aws-cdk-lib";
+import { auth } from "./auth/resource";
+import { defineBackend } from "@aws-amplify/backend";
+import { Duration } from "aws-cdk-lib";
+const backend = defineBackend({
+    auth
+});
+const cfnUserPool = backend.auth.resources.cfnResources.cfnUserPool;
+cfnUserPool.userPoolName = "gen1auth595682ee_userpool_595682ee-dev";
+cfnUserPool.usernameAttributes = undefined;
+cfnUserPool.policies = {
+    passwordPolicy: {
+        minimumLength: 8,
+        requireLowercase: false,
+        requireNumbers: false,
+        requireSymbols: false,
+        requireUppercase: false,
+        temporaryPasswordValidityDays: 7
+    }
+};
+// cfnUserPool.applyRemovalPolicy(RemovalPolicy.RETAIN, { applyToUpdateReplacePolicy: true })
+const cfnIdentityPool = backend.auth.resources.cfnResources.cfnIdentityPool;
+cfnIdentityPool.identityPoolName = "gen1auth595682ee_identitypool_595682ee__dev";
+cfnIdentityPool.allowUnauthenticatedIdentities = false;
+// cfnIdentityPool.applyRemovalPolicy(RemovalPolicy.RETAIN, { applyToUpdateReplacePolicy: true })
+const userPool = backend.auth.resources.userPool;
+userPool.addClient("NativeAppClient", {
+    disableOAuth: true,
+    authSessionValidity: Duration.minutes(3),
+    userPoolClientName: "gen1au595682ee_app_client",
+    enablePropagateAdditionalUserContextData: false,
+    enableTokenRevocation: true,
+    refreshTokenValidity: Duration.days(30)
+})
